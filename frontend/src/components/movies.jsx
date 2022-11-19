cd4d9cf24ad58f6f41b0f6ec2b9130beb105496d
Awal Amoo <awalamoo7@gmail.com>
2022-11-19 21:23:07 +0000

    adding filter table

--- frontend/src/components/movies.jsx
+++ frontend/src/components/movies.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
+import {getGenres} from "../services/fakeGenreService";
 import Like from './common/like';
+import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
 import {paginate} from '../utils/paginate';
 
 class Movies extends Component {
 	state = {
-		movies: getMovies(),
+		movies: [],
+		genres: [],
 		currentPage: 1,
 		pageSize: 4
 	};
+
+	componentDidMount() {
+		this.setState({movies: getMovies(), genres: getGenres()})
+	}
 
 	handleDelete = (movie) => {
 		const movies = this.state.movies.filter((m) => m._id !== movie._id);
@@ -24,7 +31,10 @@ class Movies extends Component {
 	};
 	handlePageChange = page => {
 		this.setState({currentPage: page});
-	}
+	};
+	handleGenreSelect = genre => {
+
+	};
 
 	
 	render() {
@@ -34,37 +44,46 @@ class Movies extends Component {
 		if (count === 0) return <p>There are no movies in the database</p>;
 		const movies = paginate(allMovies, currentPage, pageSize);
 		return (
-			<>
-				<p>Showing {count} movies in the database</p>
-				<table className="table">
-					<thead>
-						<tr>
-							<th>Title</th>
-							<th>Genre</th>
-							<th>Stock</th>
-							<th>Rate</th>
-							<th></th>
-							<th></th>
-						</tr>
-					</thead>
-					<tbody>
-						{movies.map((movie) => (
-							<tr key={movie._id}>
-								<td>{movie.title}</td>
-								<td>{movie.genre.name}</td>
-								<td>{movie.numberInStock}</td>
-								<td>{movie.dailyRentalRate}</td>
-								<td><Like
+			<div className="row">
+				<div className="col-3">
+					<ListGroup 
+					items={this.state.genres} 
+					textProperty="name"
+					valueProperty="_id"
+					onItemSelect={this.handleGenreSelect}/>
+				</div>
+
+				<div className="col">
+				 <p>Showing {count} movies in the database</p>
+				 <table className="table">
+				 	 <thead>
+						 <tr>
+							 <th>Title</th>
+							 <th>Genre</th>
+							 <th>Stock</th>
+							 <th>Rate</th>
+							 <th></th>
+							 <th></th>
+						 </tr>
+					 </thead>
+					 <tbody>
+						 {movies.map((movie) => (
+							 <tr key={movie._id}>
+								 <td>{movie.title}</td>
+								 <td>{movie.genre.name}</td>
+								 <td>{movie.numberInStock}</td>
+								 <td>{movie.dailyRentalRate}</td>
+								 <td><Like
 									 liked={movie.liked} 
 									 onClick={()=>this.handleLike(movie)}/></td>
-								<td>
-									<button
+								 <td>
+									 <button
 										onClick={() => this.handleDelete(movie)}
 										className="btn btn-danger btn-sm"
-									>
+									 >
 										Delete
-									</button>
-								</td>
+									 </button>
+								 </td>
 							</tr>
 						))}
 					</tbody>
@@ -74,9 +93,7 @@ class Movies extends Component {
 				pageSize={pageSize} 
 				currentPage={currentPage}
 				onPageChange={this.handlePageChange}/>
-			</>
-		);
-	}
-}
-
+				</div>
+			</div>
+			)}}
 export default Movies;
