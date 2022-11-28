46a737f2f81e7f3706374353c661ef9b039ce21e
Awal Amoo <awalamoo7@gmail.com>
2022-11-28 13:01:06 +0000

    add linting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import Movies from './components/movies';
-import './App.css';
+import React, { Component } from "react";
+import Movies from "./components/movies";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-     <main className='container'>
-      <Movies/>
-     </main>
-    </div>
-  );
+	return (
+		<div className="App">
+			<main className="container">
+				<Movies />
+			</main>
+		</div>
+	);
 }
 
 export default App;
