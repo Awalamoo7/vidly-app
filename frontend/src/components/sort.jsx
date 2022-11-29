7bb19fb528887101e080f76b23719b201d922f5c
Awal Amoo <awalamoo7@gmail.com>
2022-11-29 11:08:19 +0000

    sort login page added

---
+++ frontend/src/components/sort.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class  extends Component {
+    render() { 
+        return <div>
+            <h1>Login</h1>
+        </div>;
+    }
+}
+ 
+export default ;
