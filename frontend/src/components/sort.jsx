7c79278510aca73ef492960771c50576d84ea2b9
Awal Amoo <awalamoo7@gmail.com>
2022-12-01 11:24:05 +0000

    '

--- frontend/src/components/sort.jsx
+++ frontend/src/components/sort.jsx
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class  extends Component {
-    render() { 
-        return <div>
-            <h1>Login</h1>
-        </div>;
-    }
+class Sort extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Login</h1>
+			</div>
+		);
+	}
 }
- 
-export default ;
+
+export default Sort;
