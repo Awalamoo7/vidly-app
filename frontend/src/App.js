1ae0126dac60a5a345c6bcc49c19161c6ac4b144
Awal Amoo <awalamoo7@gmail.com>
2022-12-24 03:18:18 +0000

    added navbar and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import NavBar from "./components/navBar";
+import Customers from "./components/customers";
+import MovieForm from "./components/movieForm";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 import "./App.css";
-
-function App() {
-	return (
-		<div className="App">
-			<main className="container">
-				<Movies />
-			</main>
-		</div>
-	);
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<NavBar />
+				<main className="container">
+					<Switch>
+						<Route path="/movies/:id" component={MovieForm}></Route>
+						<Route path="/movies" component={Movies}></Route>
+						<Route path="/customers" component={Customers}></Route>
+						<Route path="/rentals" component={Rentals}></Route>
+						<Route path="/not-Found" component={NotFound}></Route>
+						<Redirect from="/" exact to="/movies" />
+						<Redirect to="/not-Found" />
+					</Switch>
+				</main>
+			</div>
+		);
+	}
 }
 
 export default App;
